f52a5f4fb0091a68be624d39c3c7c0e74d1079da
pisarews <pisaqtom@gmail.com>
2020-08-05 22:02:15 +0100

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using RestoCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Resto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            boxHeight.Text = "50";
+            boxwidth.Text = "50";
+            
+            grid = MyGrid; 
+        }
+
+        public static RestaurantTable MyGridstatic { get; set;  }
+        public static Grid grid { get; set; }
+        
+
+        private void boxHeight_TextChanged(object sender, TextChangedEventArgs e)
+        {
+                if (boxHeight.Text != null && boxHeight.Text != "" && RestaurantTable.actualSelectedTable != null)
+                RestaurantTable.actualSelectedTable.Height = Convert.ToDouble(boxHeight.Text); 
+        }
+
+        private void boxwidth_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (boxwidth.Text != null && boxwidth.Text != "" && RestaurantTable.actualSelectedTable != null)
+                RestaurantTable.actualSelectedTable.Width = Convert.ToDouble(boxwidth.Text);
+        }
+
+        private void RadioButtonYes_Checked(object sender, RoutedEventArgs e)
+        {
+            if (RadioButtonNo != null)
+            {
+                RestaurantTable.actualSelectedTable.Reservation_Enable(); 
+            }            
+        }
+
+        private void RadioButtonNo_Checked(object sender, RoutedEventArgs e)
+        {
+            RestaurantTable.actualSelectedTable.Reservation_Disable(); 
+        }
+
+        private void NumberOfSeats_Box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(NumberOfSeats_Box.Text != null && NumberOfSeats_Box.Text != "")
+                RestaurantTable.actualSelectedTable.UpdateNumberOfSeats(Convert.ToInt32(NumberOfSeats_Box.Text)); 
+        }
+
+        private void NewTable_Click(object sender, RoutedEventArgs e)
+        {
+            RestaurantTable table = new RestaurantTable();
+            table.Rectus.Width = 50;
+            table.Rectus.Height = 50;
+            table.HorizontalAlignment = HorizontalAlignment.Right;  
+            grid.Children.Add(table);
+            Client.CreateClients(RestaurantTable.actualSelectedTable.numberOfSeats, RestaurantTable.actualSelectedTable.clients); 
+        }
+    }
+}
---
+++ RestaurantTable.xaml.cs
@@ -0,0 +1,123 @@
+﻿using RestoCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Resto
+{
+    /// <summary>
+    /// Logika interakcji dla klasy RestaurantTable.xaml
+    /// </summary>
+    public partial class RestaurantTable : UserControl
+    {
+        public RestaurantTable()
+        {
+            InitializeComponent();
+
+            if (isReserved == true)
+            {
+                Rectus.Background = Brushes.Red;
+            }
+            else
+            {
+                Rectus.Background = Brushes.Green;
+            }
+            
+        }
+        
+        public bool isReserved { get; set; }
+        public int numberOfSeats { get; set;}
+        public int NumberOfTable { get; set; }
+        public bool isMove { get; set; }
+        public PropertyChangedCallback PropertyChanged;
+        public List<Client> clients = new List<Client>(); 
+
+        public static RestaurantTable actualSelectedTable { get; set; }
+
+        private Point? _buttonPosition;
+        private double deltaX;
+        private double deltaY;
+        private bool _isMoving;
+        private TranslateTransform _currentTT;
+
+        public void Reservation_Enable()
+        {
+            this.Rectus.Background = Brushes.Red;
+            this.isReserved = true;       
+        }
+
+        public void Reservation_Disable()
+        {
+            this.Rectus.Background = Brushes.Green;
+            this.isReserved = false;
+        }
+
+        public void UpdateNumberOfSeats(int x)
+        {
+            Rectus.Content = x.ToString(); 
+        }
+
+        private void Rectus_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            actualSelectedTable = this; 
+            MainWindow.MyGridstatic = this;
+            if (_buttonPosition == null)
+                _buttonPosition = Rectus.TransformToAncestor(MainWindow.MyGridstatic).Transform(new Point(0, 0));
+            var mousePosition = Mouse.GetPosition(MainWindow.MyGridstatic);
+            deltaX = mousePosition.X - _buttonPosition.Value.X;
+            deltaY = mousePosition.Y - _buttonPosition.Value.Y;
+            _isMoving = true;
+        }
+
+        private void Rectus_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _currentTT = Rectus.RenderTransform as TranslateTransform;
+            _isMoving = false;
+        }
+
+        private void Rectus_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isMoving) return;
+
+            var mousePoint = Mouse.GetPosition(MainWindow.MyGridstatic);
+
+            #region axe X 
+            var offsetX = (_currentTT == null ? _buttonPosition.Value.X : _buttonPosition.Value.X - _currentTT.X) + deltaX - mousePoint.X;
+            
+            if ( offsetX < 0)
+            {
+                offsetX = 0; 
+            }
+            if(offsetX > MainWindow.grid.ActualWidth)
+            {
+                offsetX = MainWindow.grid.ActualWidth; 
+            }
+            #endregion
+
+
+            #region YAxe 
+            var offsetY = (_currentTT == null ? _buttonPosition.Value.Y : _buttonPosition.Value.Y - _currentTT.Y) + deltaY - mousePoint.Y;
+            if (offsetY < 0 -MainWindow.grid.ActualHeight / 2) offsetY = 0 - MainWindow.grid.ActualHeight / 2;
+            if (offsetY > MainWindow.grid.ActualHeight) offsetY = MainWindow.grid.ActualHeight;
+            #endregion
+            this.Rectus.RenderTransform = new TranslateTransform(-offsetX, -offsetY);
+        }
+
+        private void Rectus_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.MyGridstatic = this;
+        }
+    }
+}
